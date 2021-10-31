66cdfa895f7f5132b8ea49294c46e418570aa2a8
mikeanthony23 <mikeanthony595@gmail.com>
2021-10-31 23:37:16 +0800

    remove debounce function from class and transferred to helpers

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,26 +1,17 @@
 import View from './view'
 import { SEARCH_DELAY_MS } from '../config'
+import { debounce } from '../helpers'
 
 class SearchView extends View {
   #inputWrapper = document.querySelector('.input')
 
-  #debounce(func, delay) {
-    let timer
-    return function (...args) {
-      clearTimeout(timer)
-      timer = setTimeout(() => {
-        func(...args)
-      }, delay)
-    }
-  }
-
   addHandlerSearch(handler) {
-    const debounce = this.#debounce(handler, SEARCH_DELAY_MS)
+    const searchDebounce = debounce(handler, SEARCH_DELAY_MS)
     if (!this.#inputWrapper) return
-    this.#inputWrapper.addEventListener('input', e => {
+    this.#inputWrapper.addEventListener('input', function (e) {
       const target = e.target.closest('.input__search')
       if (!target) return
-      debounce()
+      searchDebounce()
     })
   }
 
